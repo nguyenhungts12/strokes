d49b7fe2f4a97198e1d7cbdf46f0099d500db972
nguyenhungts12 <nguyenhung.ts12@gmail.com>
2022-03-27 10:00:36 +0700

    ok commit

--- auto.bat
+++ auto.bat
@@ -1,8 +1,13 @@
-git add *
-git commit -m "6 commit"
-git push
-pause
+::git add *
+::git commit -m "6 commit"
+::git push
+::pause
+ECHO OFF
 
-FOR %F IN (E:\strokes\*.*) DO (
-  ECHO !SrcCount! COPY %F C:\temp\output
+FOR /r E:\strokes %%F IN (*.gif) DO (
+  ECHO %%F
+  move %%F
+  git add *
+  git commit -m "ok commit"
+  git push
 )
